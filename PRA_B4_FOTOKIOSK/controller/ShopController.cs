a04213c07cf100dbf6e5c115af7873f95717ddef
Galaxy <62033325+GalaxyComic@users.noreply.github.com>
2024-05-29 08:59:13 +0200

    bug fix vergeten paramater van functie

--- PRA_B4_FOTOKIOSK/controller/ShopController.cs
+++ PRA_B4_FOTOKIOSK/controller/ShopController.cs
@@ -55,7 +55,7 @@ namespace PRA_B4_FOTOKIOSK.controller
         // Wordt uitgevoerd wanneer er op de Save knop is geklikt
         public void SaveButtonClick()
         {
-            ShopManager.AddShopReceipt();
+            ShopManager.AddShopReceipt("Bon Gemaakt!");
         }
 
     }
